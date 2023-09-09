8f0bff097a416d27da9955d013e1831b2345a140
Richard Carson <r.carson@calian.com>
2023-09-08 23:52:23 -0400

    actually fix tests this time

--- test/lavendeux/decorator.test.js
+++ test/lavendeux/decorator.test.js
@@ -1,5 +1,5 @@
 import { test, expect, describe } from 'vitest';
-import { LavendeuxValue } from '../../src/lavendeux/value';
+import { Types } from '../../src/lavendeux/value';
 import { LavendeuxDecorator } from '../../src/lavendeux/decorator';
 import { Lavendeux } from '../../src/lavendeux/index';
 
@@ -8,19 +8,20 @@ new Lavendeux('test', 'test');
 
 describe('LavendeuxDecorator', () => {
     test('constructor', () => {
-        let func = new LavendeuxDecorator('test', LavendeuxValue.Types.Any, () => {});
+        let func = new LavendeuxDecorator('test', Types.Any, () => {});
         expect(globalThis.lavendeuxdecorator_test_args.length).toBe(1);
-        expect(func.returnType).toBe(LavendeuxValue.Types.Any);
+        expect(func.returnType).toBe(Types.String);
+        expect(func.args[0].type).toBe(Types.Any);
     });
     
     test('registeredName', () => {
-        let func = new LavendeuxDecorator('test', LavendeuxValue.Types.Any, () => {});
+        let func = new LavendeuxDecorator('test', Types.Any, () => {});
         expect(func.registeredName()).toBe('lavendeuxdecorator_test');
     });
     
     test('register', () => {
         let result = false;
-        let func = new LavendeuxDecorator('test', LavendeuxValue.Types.Any, () => {result = true;});
+        let func = new LavendeuxDecorator('test', Types.Any, () => {result = true;});
         expect(globalThis.extensionDetails.decorators.test).toBe('lavendeuxdecorator_test');
         expect(typeof globalThis.lavendeuxdecorator_test).toBe('function');
         expect(globalThis.lavendeuxdecorator_test_args.length).toBe(1);
